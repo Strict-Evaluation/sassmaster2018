c30ae057b23b2ae465d267a991095abf49092651
Cheekyshroom <phernima@ucsc.edu>
2018-01-20 15:57:34 -0500

    Made parse.lua a module

--- parse.lua
+++ parse.lua
@@ -1,38 +1,39 @@
-#!/usr/bin/env th
--- thx peter
+local module = {}
 
-open = io.open
-function read_file(path)
-    local file = open(path, "r") -- r read mode and b binary mode
-    local lines = {}
+function module.read_file(path)
+  local open = io.open
+  local file = open(path, "r") -- r read mode and b binary mode
+  local lines = {}
 
-    for line in io.lines(path) do
-        local corpus,
-        label,
-        id,
-        quotetext,
-        responsetext = line:match("([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)")
-        local quottable = {}
-        local resptable = {}
-        if quotetext then
-            local quottable = string.lower(quotetext):gsub(".",function(x) table.insert(quottable, string.byte(x)) end)
-        end
-        if responsetext then
-            local resptable = string.lower(responsetext):gsub(".",function(x) table.insert(resptable, string.byte(x)) end)
-        end
-        lines[#lines+1] = { corpus = corpus,
-                            label = label,
-                            id = id,
-                            quottable = quottable,
-                            responsetable = resptable
-                          }
+  for line in io.lines(path) do
+      local corpus,
+      label,
+      id,
+      quotetext,
+      responsetext = line:match("([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)")
+      local quottable = {}
+      local resptable = {}
+      if quotetext then
+          local quottable = string.lower(quotetext):gsub(".",function(x) table.insert(quottable, string.byte(x)) end)
+      end
+      if responsetext then
+          local resptable = string.lower(responsetext):gsub(".",function(x) table.insert(resptable, string.byte(x)) end)
+      end
+      lines[#lines+1] = { corpus = corpus,
+                          label = label,
+                          id = id,
+                          quottable = quottable,
+                          responsetable = resptable
+                        }
 
-    end
-    file:close()
-    return lines
+  end
+  file:close()
+  return lines
 end
 
-lines = read_file("sarcasm_v2.csv")
-print (lines[20])
+return module
 
-return 0;
+-- lines = read_file("sarcasm_v2.csv")
+-- print (lines[20])
+
+--return 0;
