a42c34f83753e0c4177c512923c95f1454cad251
Cheekyshroom <phernima@ucsc.edu>
2018-01-20 15:44:54 -0500

    Pushing broken server + training run.lua

--- run.lua
+++ run.lua
@@ -1,16 +1,40 @@
 #!/usr/bin/env th
 
+require 'torch'
+
 -- Train our network
 
 
 -- Now handle requests!
 
-local f = assert(io.open('ml_fifo', "r"))
-local line = f:read()
+if arg[1] == 'train' then
+  -- test network here
 
-while line ~= "quit" and line ~= "" do
-  print("Woah!" .. line)
-  line = f:read("*all")
+  print('Testing:')
+
+  os.exit()
 end
 
-f:close()
+local socket = require 'socket'
+local s = assert(socket.bind('localhost', 12345))
+local c = assert(s:accept())
+
+while true do
+  print('iterating')
+  local line = c:receive()
+  if line then
+    print('got', line)
+    from:send("helo!\n")
+    print('written')
+  else
+    print("nil line")
+  end
+  print('here')
+  if line == 'quit' then 
+    print('there')
+    from:send("You quit!\n")
+    break
+  end
+  print('finished loop')
+end
+
